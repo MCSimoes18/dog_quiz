450c2f1abeb895bd92c2f2c17153342030ebc8d8
Maria Cristina Simoes <MariaCristinaSimoes@Marias-MBP.home>
2018-11-11 17:41:55 -0500

    add pictures and styling

--- dogs.js
+++ dogs.js
@@ -1,4 +1,4 @@
-alert("Connected!");
+
 
 var age = document.querySelector(".questionAge");
 var temper = document.querySelector(".questionTemper");
@@ -83,40 +83,66 @@ questionOne();
 questionTwo();
 questionThree();
 
+
 function findPuppy(age, temper,fur){
 if (age == "old" && temper == "calm" && fur == "long"){
-	$('#result').html("<h2>Zuchhini is a friendly dog</h2>")
+	$('#result').html("<h2 class='result-text'>You matched with Zucchini! </h2> <br> <p class='result-text'>Zucchini is a calm dog that is believed to be a Jack Russell mix. She is very affectionate.<p>")
+	$('img').hide()
+	$('#zucchini').show()
+	
 }
 
 else if (age == "old" && temper == "calm"  && fur == "short"){
-console.log("Kilian")
+	$('#result').html("<h2 class='result-text'>You matched with Kilian</h2> <br> <p class='result-text'>Kilian is a sweet, shy dog that loves being outside. He could roll around on grass for hours!<p>")
+	$('img').hide()
+	$('#kilian').show()
+	
 }
 
 else if (age == "old" && temper == "playful" && fur =="long"){
-console.log("Dobby")
+	$('#result').html("<h2 class='result-text'>You matched with Dobby!</h2> <br> <p class='result-text'>Dobby is a Chihuahua who is super playful and loves to cuddle. She leaps on to your lap whenever she gets a chance. <p>")
+	$('img').hide()
+	$('#dobby').show()
+	
 }
 
 else if (age == "old" && temper == "playful" && fur =="short"){
-console.log("Millie")
+	$('#result').html("<h2 class='result-text'> You matched with Millie! </h2> <p class='result-text'> Millie is a Pit/Lab/German Shepherd Mix who is very sweet, smart and playful. She is super friendly and loves all people, other dogs and food. She is also good a learning new tricks! <p><br>")
+	$('img').hide()
+	$('#millie').show()
+	
 }
 
 if (age == "puppy" && temper == "calm" && fur == "long"){
-console.log("Ren")
+	$('#result').html("<h2 class='result-text'> You matched with Ren! </h2> <p class='result-text'> Ren is a sweet, shy Australian Shepherd mix. He loves belly rubs. He is also full of surprises and climbs on top of his crate! <p><br>")
+	$('img').hide()
+	$('#ren').show()
+
 }
 
 else if (age == "puppy" && temper == "calm"  && fur == "short"){
-console.log("Athena")
+	$('#result').html("<h2 class='result-text'>You matched with Athena! </h2> <p class='result-text'> Athena is a sweet, calm, puppy. <p>")
+	$('img').hide()
+	$('#athena').show()
+
 }
 
 else if (age == "puppy" && temper == "playful" && fur =="long"){
-console.log("Murphy")
+	$('#result').html(" <h2 class='result-text'>You matched with Murphy </h2> <p class='result-text'>Murphy is an energetic, spunky pup! He loves to play, and you can often find him hiding in peculiar spaces.")
+	$('img').hide()
+	$('#murphy').show()
+	
 }
 
 else if (age == "puppy" && temper == "playful" && fur =="short"){
-console.log("Penelope")
+	$('#result').html("<h2 class='result-text'>You matched with Penelope!</h1> <p class='result-text'> Penelope is a sweet pup who LOVES to cuddle and be around people.<p>")
+	$('img').hide()
+	$('#penelope').show()
 
 }}
 
+
 form.addEventListener("submit", function() {
 findPuppy(age,temper,fur);
+
 });
